ebaac0e981f7b7fe83485218e724164d60d00901
Ryouy <105139695+ryouy@users.noreply.github.com>
2024-06-12 22:44:40 +0900

    shorter version

--- ex1_random_walk_triangle/random_walk_triangle.c
+++ ex1_random_walk_triangle/random_walk_triangle.c
@@ -9,54 +9,35 @@
 
 const int N = 100000;
 float TriangleDots[N+2][2];
-/*  for better visualization
-float BigTriangles[3][2] = {
-    {0.05f, 0.05f},
-    {0.95f, 0.05f},
-    {0.5f, 0.95f}
-};
-*/
 
 void LetsFractal(int N) {
-    TriangleDots[0][0] = 0.0f;
-    TriangleDots[0][1] = 0.0f;
-    TriangleDots[1][0] = 1.0f;
-    TriangleDots[1][1] = 0.0f;
-    TriangleDots[2][0] = 0.5f;
-    TriangleDots[2][1] = 1.0f;
+    TriangleDots[0][0] = 0.0f; TriangleDots[0][1] = 0.0f;
+    TriangleDots[1][0] = 1.0f; TriangleDots[1][1] = 0.0f;
+    TriangleDots[2][0] = 0.5f; TriangleDots[2][1] = 1.0f;
 
     for (int i = 3; i < N+2; i++) {
         int random = rand() % 3;
-        TriangleDots[i][0] = (TriangleDots[i - 1][0] + TriangleDots[random][0]) / 2;
-        TriangleDots[i][1] = (TriangleDots[i - 1][1] + TriangleDots[random][1]) / 2;
+        for (int j = 0; j < 2; j++)
+            TriangleDots[i][j] = (TriangleDots[i - 1][j] + TriangleDots[random][j]) / 2;
     }
 }
-
+void LetsDisplay(float color, int n,int N, int a, int b){
+    glPointSize(color);
+    glColor3f(a, b, 0);
+    glBegin(GL_POINTS);
+    for (int i = n; i < N; i++) glVertex2fv(TriangleDots[i]);
+    glEnd();
+}
 static void display(void) {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-
     // Repaint the background with the color
     // specified by glClearColor()
     glClear(GL_COLOR_BUFFER_BIT);
 
     LetsFractal(N);
-
-    glPointSize(10.0f);
-    glBegin(GL_POINTS);
-    for (int i = 0; i < 3; i++) {
-        glColor3f(0, 1, 0); // Set color to green
-        glVertex2fv(TriangleDots[i]);
-    }
-    glEnd();
-
-    glPointSize(2.0f);
-    glBegin(GL_POINTS);
-    for (int i = 3; i < N+2; i++) {
-        glColor3f(1, 0, 0); // Set color to red
-        glVertex2fv(TriangleDots[i]);
-    }
-    glEnd();
+    LetsDisplay(10.0f, 0, 3, 0, 1);
+    LetsDisplay(2.0f, 3, N+2, 1, 0);
 
     glutSwapBuffers();
 }
